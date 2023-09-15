9e8046d0b90fda8e96ac4cdd6d1450e011013bf9
EllieKosowsky <89863365+EllieKosowsky@users.noreply.github.com>
2023-09-15 01:29:52 -0500

    Simple Movement

---
+++ Assets/Code/SimpleMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    //Outlet
+    Rigidbody2D _rigidbody2D;
+
+    //Methods
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //Move Player Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody2D.AddForce(Vector2.left * 5f * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        //Move Player Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody2D.AddForce(Vector2.right * 5f * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        //Move Player Up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _rigidbody2D.AddForce(Vector2.up * 5f * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        //Move Player Down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _rigidbody2D.AddForce(Vector2.down * 5f * Time.deltaTime, ForceMode2D.Impulse);
+        }
+    }
+}
