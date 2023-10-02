306ce74050985bc22c18f5679e3113ee85af5812
nmclaman <89808041+nmclaman@users.noreply.github.com>
2023-10-02 16:39:05 -0500

    W5 Recommit
    My commit on saturday didn't fully resolve, so I pulled most recent and re-added my stuff

---
+++ Assets/Code/SubmarineHealth.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineHealth : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth;
+    public HealthBarScript healthBar;
+    private int damage;
+    private float velocityDifference;
+    public int impactScalar;
+    public float scrapeScalar;
+    public float scrapeSense;
+    public GameObject target;
+    public int pointValue;
+
+            void Start()
+            {
+                 currentHealth = maxHealth;
+                 healthBar.SetMaxHealth(maxHealth);
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                if (currentHealth <= 0) {
+
+                    GameObject.Find("Timer").GetComponent<ScoreSystem>().AddPoint(pointValue);
+                   // target.GetComponent<ScoreSystem>().points = target.GetComponent<ScoreSystem>().points - pointValue;
+                    //ScoreSystem.AddPoint(pointValue);
+                    print(target.GetComponent<ScoreSystem>().points);
+
+
+                    Destroy(gameObject);
+
+                }
+            }
+            void OnCollisionEnter2D(Collision2D other){
+                /* if (other.gameObject.GetComponent<SimpleMovement>())
+                 {*/
+                velocityDifference = other.relativeVelocity.magnitude;
+                    currentHealth = currentHealth - velocityDifference*impactScalar;
+                    healthBar.SetHealth(currentHealth);
+              //  Debug.Log(currentHp);
+              // }
+            }
+            void OnCollisionStay2D(Collision2D other)
+            {
+                velocityDifference = other.relativeVelocity.magnitude;
+                if (velocityDifference > scrapeSense)
+                {
+                    currentHealth = currentHealth - velocityDifference * scrapeScalar * Time.deltaTime;
+                   healthBar.SetHealth(currentHealth);
+                   // Debug.Log(currentHp);
+                }
+            }
+        }
+
